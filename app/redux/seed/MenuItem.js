77b44b70c86d92890a279f03743c79f2480641ac
Tom Schuster <tschuster@taskstream.com>
2017-02-16 11:40:20 -0500

    Update MenuItem.js

--- app/redux/seed/MenuItem.js
+++ app/redux/seed/MenuItem.js
@@ -1,5 +1,4 @@
-
-export const menuItem: [
+export const menuItem = [
           { ItemId: 1, PlaceId: 1, ItemName: 'Shack Burger'},
 		  { ItemId: 2, PlaceId: 1, ItemName: 'Shroom Burger'},
 		  { ItemId: 3, PlaceId: 1, ItemName: 'Hamburger'},
