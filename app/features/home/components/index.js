ab1a42ed0e3cb9700f0cbab9d5dcb822dd8b95bf
Tom Schuster <tomjschuster@gmail.com>
2017-02-16 11:13:13 -0500

    change object format

--- app/features/home/components/index.js
+++ app/features/home/components/index.js
@@ -4,22 +4,22 @@ import Paper from 'material-ui/Paper'
 export default class Home extends Component {
   constructor(props) {
     super(props)
-    this.state = { restaurant: {} }
+    this.state = { place: {} }
   }
 
   componentDidMount() {
     const input = this.refs.autocomplete
     const autocomplete = new google.maps.places.Autocomplete(input)
     autocomplete.addListener('place_changed', () => {
-          const place = autocomplete.getPlace()
-          const restaurant = {
-            restaurantId: place.id,
-            name: place.name,
-            address: place.formatted_address || '',
-            photos: place.photos || []
+          const googlePlace = autocomplete.getPlace()
+          const place = {
+            PlaceId: googlePlace.id,
+            Name: googlePlace.name,
+            Address: googlePlace.formatted_address || '',
+            Photos: googlePlace.photos || []
           }
           console.log(place)
-          this.setState({ restaurant })
+          this.setState({ place })
     })
   }
 
@@ -28,7 +28,7 @@ export default class Home extends Component {
       <div>
         <input ref='autocomplete' />
         <Paper>
-          { JSON.stringify(this.state.restaurant)}
+          { JSON.stringify(this.state.place)}
         </Paper>
       </div>
     )
