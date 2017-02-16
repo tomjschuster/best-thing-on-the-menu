131bca403dcacd21528447a7c9778965969ea7ba
Tom Schuster <tomjschuster@gmail.com>
2017-02-16 11:04:49 -0500

    autocomplete functional

--- app/features/home/components/index.js
+++ app/features/home/components/index.js
@@ -2,10 +2,34 @@ import React, { Component } from 'react'
 import Paper from 'material-ui/Paper'
 
 export default class Home extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { restaurant: {} }
+  }
+
+  componentDidMount() {
+    const input = this.refs.autocomplete
+    const autocomplete = new google.maps.places.Autocomplete(input)
+    autocomplete.addListener('place_changed', () => {
+          const place = autocomplete.getPlace()
+          const restaurant = {
+            restaurantId: place.id,
+            name: place.name,
+            address: place.formatted_address || '',
+            photos: place.photos || []
+          }
+          console.log(place)
+          this.setState({ restaurant })
+    })
+  }
+
   render() {
     return (
       <div>
-        <Paper style={{width: 300, height: 300}}/>
+        <input ref='autocomplete' />
+        <Paper>
+          { JSON.stringify(this.state.restaurant)}
+        </Paper>
       </div>
     )
   }
