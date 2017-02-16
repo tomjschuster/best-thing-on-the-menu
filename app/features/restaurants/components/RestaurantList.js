de0cdce57b3760169af058c20337e8f95391a2b4
Narie Lee <nlee@taskstream.com>
2017-02-16 12:39:06 -0500

    changes

--- app/features/restaurants/components/RestaurantList.js
+++ app/features/restaurants/components/RestaurantList.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router'
 import Paper from 'material-ui/Paper'
-import RestaurantCard from './RestaurantCard'
 import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card'
 import FlatButton from 'material-ui/FlatButton'
 
--- app/features/restaurants/components/index.js
+++ app/features/restaurants/components/index.js
@@ -4,7 +4,6 @@ import IconButton from 'material-ui/IconButton'
 import Subheader from 'material-ui/Subheader'
 import StarBorder from 'material-ui/svg-icons/toggle/star-border'
 import Paper from 'material-ui/Paper'
-import RestaurantCard from './RestaurantCard'
 import RestaurantList from './RestaurantList'
 
 const Restaurants = props => (
