f4bfbd4e8112067a8f5c95fb6bbb19899ad6439f
Tom Schuster <tschuster@taskstream.com>
2017-02-16 11:42:18 -0500

    Update Review.js

--- app/redux/seed/Review.js
+++ app/redux/seed/Review.js
@@ -1,13 +1,11 @@
-
-export const review: [
-		{ ReviewId: 1, Stars: 4, ItemId: 1, PersonID: 61, Comment: 'Forever favorite burger for New Yorker!' },
-		{ ReviewId: 2, Stars: 2, ItemId: 1, PersonID: 64, Comment: 'Almost every time I go, they get my order wrong; probably because the waiter is walking away before I even finish explaining what I want.'  },
-		{ ReviewId: 3, Stars: 3, ItemId: 1, PersonID: 2, Comment: 'I ordered the burger it was bland. The fries were all right. I ordered the frozen hot chocolate, it was magnificent.  It is the only reason I have a three star review. '},
-		{ ReviewId: 4, Stars: 5, ItemId: 5, PersonID: 2, Comment: 'So I'm going to start off with saying this is one of my favorite places to eat. Their Orange Chicken and Tom Yum soup is my lunch special about 3 times a week. ' },
-		{ ReviewId: 5, Stars: 4, ItemId: 5, PersonID: 7, Comment:'Overall, the food has decent flavors, is served in large portions, and the ambiance is decent' },
-		{ ReviewId: 6, Stars: 4, ItemId: 5, PersonID: 55, Comment:'The food was good and the service was even better! I left my phone at the restaurant and they saved it for me! ' },
-		{ ReviewId: 7, Stars: 5, ItemId: 5, PersonID: 76, Comment:'Lunch specials are bombbbbb.' },
-		{ ReviewId: 8, Stars: 5, ItemId: 5, PersonID: 78, Comment:'I was sitting next to someone at school who was eating some food that smelled so good that I couldn't resist the urge to ask him where he bought it from. Bless his heart for not getting creeped out, and for telling me about this restaurant's lunch special. My life will never be the same.' },
-		{ ReviewId: 9, Stars: 1, ItemId: 5, PersonID: 23, Comment:'How is this place so popular? I ordered in from one of their locations last night, and I was up all night not feeling well.' }	
-
+export default [
+		{ ReviewId: 1, Stars: 4, ItemId: 1, PersonId: 61, Comment: 'Forever favorite burger for New Yorker!' },
+		{ ReviewId: 2, Stars: 2, ItemId: 1, PersonId: 64, Comment: 'Almost every time I go, they get my order wrong; probably because the waiter is walking away before I even finish explaining what I want.'  },
+		{ ReviewId: 3, Stars: 3, ItemId: 1, PersonId: 2, Comment: 'I ordered the burger it was bland. The fries were all right. I ordered the frozen hot chocolate, it was magnificent.  It is the only reason I have a three star review. '},
+		{ ReviewId: 4, Stars: 5, ItemId: 5, PersonId: 2, Comment: 'So I'm going to start off with saying this is one of my favorite places to eat. Their Orange Chicken and Tom Yum soup is my lunch special about 3 times a week. ' },
+		{ ReviewId: 5, Stars: 4, ItemId: 5, PersonId: 7, Comment:'Overall, the food has decent flavors, is served in large portions, and the ambiance is decent' },
+		{ ReviewId: 6, Stars: 4, ItemId: 5, PersonId: 55, Comment:'The food was good and the service was even better! I left my phone at the restaurant and they saved it for me! ' },
+		{ ReviewId: 7, Stars: 5, ItemId: 5, PersonId: 76, Comment:'Lunch specials are bombbbbb.' },
+		{ ReviewId: 8, Stars: 5, ItemId: 5, PersonId: 78, Comment:'I was sitting next to someone at school who was eating some food that smelled so good that I couldn't resist the urge to ask him where he bought it from. Bless his heart for not getting creeped out, and for telling me about this restaurant's lunch special. My life will never be the same.' },
+		{ ReviewId: 9, Stars: 1, ItemId: 5, PersonId: 23, Comment:'How is this place so popular? I ordered in from one of their locations last night, and I was up all night not feeling well.' }
 ]
