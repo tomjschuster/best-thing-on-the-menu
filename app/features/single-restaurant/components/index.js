507bad4af20114527abd44868600dd4e017f47b0
Tom Schuster <tomjschuster@gmail.com>
2017-02-24 06:59:35 -0500

    add review elements

--- app/features/single-restaurant/components/index.js
+++ app/features/single-restaurant/components/index.js
@@ -1,12 +1,34 @@
 import React, { Component } from 'react'
-import FlatButton from 'material-ui/FlatButton'
-import DropDownMenu from 'material-ui/DropDownMenu'
-import AutoComplete from 'material-ui/AutoComplete'
-import MenuItem from 'material-ui/MenuItem'
-import { Card, CardActions, CardHeader, CardTitle, CardText } from 'material-ui/Card'
+
 import Item from './Item'
+
+import AutoComplete from 'material-ui/AutoComplete'
+import IconButton from 'material-ui/IconButton'
+import { Card, CardActions, CardTitle, CardText } from 'material-ui/Card'
+import TextField from 'material-ui/TextField'
+import RaisedButton from 'material-ui/RaisedButton'
+import Paper from 'material-ui/Paper'
+import Star from 'material-ui/svg-icons/toggle/star'
+import StarBorder from 'material-ui/svg-icons/toggle/star-border'
+
 import { denormalizeRestaurants } from '../../../utils'
 import { itemAutoCompleteDataConfig } from '../../../config'
+import { times } from 'lodash'
+
+
+const RatingStars = ({ starCount, onClick }) => (
+    <div>
+      { Array(5).fill(0).map((_, idx) => (
+            <IconButton
+              key={`star-${idx}`}
+              onClick={ () => onClick(idx) }>
+              { starCount <= idx ? <StarBorder /> : <Star /> }
+            </IconButton>
+          )
+        )
+      }
+    </div>
+  )
 
 export default class SingleRestaurant extends Component {
   constructor(props) {
@@ -28,6 +50,7 @@ export default class SingleRestaurant extends Component {
             receiveCurrentRestaurant,
             router
           } = this.props
+          console.log(times(5, (...args) => console.log(...args)))
 
     const denormRestaurants = denormalizeRestaurants( restaurants,
                                                       items,
@@ -59,7 +82,7 @@ export default class SingleRestaurant extends Component {
       addItemToCurrentRestaurant({...item, reviews: []})
     }
   }
-  onSelectStar = (event, index, value) => this.setState({ stars: value})
+  setStars = (idx) => this.setState({ stars: idx + 1})
   onSelectItem = (event, index, value) => this.setState({ item: value})
   onChangeItemName = (...args) => console.log(...args)
   onChangeComment = (event) => this.setState({ comment: event.target.value})
@@ -77,23 +100,49 @@ export default class SingleRestaurant extends Component {
 
 
   render() {
-    const { onChangeItemName, onItemSelect } = this
+    const { onChangeItemName, onItemSelect, setStars } = this
     const { currentRestaurant: { name, address, items } } = this.props
+    const { stars } = this.state
 
     return (
       <Card>
         <CardTitle title={name} subtitle={address} />
-        <CardHeader>
-          <AutoComplete
-            floatingLabelText='Select or enter a menu item.'
-            openOnFocus
-            filter={AutoComplete.caseInsensitiveFilter}
-            dataSource={items || []}
-            dataSourceConfig={itemAutoCompleteDataConfig}
-            onNewRequest={onItemSelect}
-            onUpdateInput={onChangeItemName}
-          />
-        </CardHeader>
+        <CardActions>
+            <RaisedButton label='Add a Review' primary={true} />
+            <Paper>
+              <ul style={{listStyle: 'none'}}>
+                <li>
+                  <AutoComplete
+                    floatingLabelText='Select or enter a menu item.'
+                    openOnFocus
+                    filter={AutoComplete.caseInsensitiveFilter}
+                    dataSource={items || []}
+                    dataSourceConfig={itemAutoCompleteDataConfig}
+                    onNewRequest={onItemSelect}
+                    onUpdateInput={onChangeItemName}
+                    style={{paddingLeft: '12px'}}
+                  />
+                </li>
+                <li>
+                  <RatingStars
+                    starCount={stars}
+                    onClick={setStars}
+                  />
+                </li>
+                <li>
+                  <TextField
+                    name='comment-field'
+                    multiLine
+                    rows={3}
+                    floatingLabelText='Comment'
+                    style={{paddingLeft: '12px'}}
+                  />
+                </li>
+              </ul>
+              <RaisedButton label='Submit' primary={true} />
+              <RaisedButton label='Cancel' primary={true} />
+            </Paper>
+        </CardActions>
         <CardText>
           { items && items.map(item => <Item key={item.id} item={item} />) }
         </CardText>
