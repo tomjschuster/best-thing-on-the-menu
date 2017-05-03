134bf5082ff88b8ee69d4c8e4f032b811ed6a756
Tom Schuster <tomjschuster@gmail.com>
2017-05-03 03:35:29 -0400

    update exclude path

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
     loaders: [
       {
         test: /\.js$/,
-        exclude: 'node_modules',
+        exclude: '/node_modules',
         loader: 'babel-loader',
         query: {
           presets: [ 'react', 'es2015', 'stage-0']
