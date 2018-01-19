39f9cb10f88ed21c3d607010ed00a1349246a3f9
Tom Schuster <tomjschuster@gmail.com>
2018-01-19 17:19:36 -0500

    import not require

--- client/components/index.js
+++ client/components/index.js
@@ -1,12 +1,17 @@
 import { connect } from 'react-redux'
 
+import Main from './Main'
+import Login from './Login'
+import Explore from './Explore'
+import SinglePlace from './SinglePlace'
+import ErrorPage from './ErrorPage'
 
 const components = {
-  Main: require('./Main/index.js').default,
-  Login: require('./Login/index.js').default,
-  Explore: require('./Explore/index.js').default,
-  SinglePlace: require('./SinglePlace/index.js').default,
-  ErrorPage: require('./ErrorPage/index.js').default
+  Main,
+  Login,
+  Explore,
+  SinglePlace,
+  ErrorPage
 }
 
 export default
