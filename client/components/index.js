ff4c5de5bcfc855772e64caea2f755fee6c918af
Tom Schuster <tomjschuster@gmail.com>
2018-01-19 17:17:03 -0500

    correct file names

--- client/components/index.js
+++ client/components/index.js
@@ -2,11 +2,11 @@ import { connect } from 'react-redux'
 
 
 const components = {
-  Main: require('./Main.js').default,
-  Login: require('./Login.js').default,
-  Explore: require('./Explore.js').default,
-  SinglePlace: require('./SinglePlace.js').default,
-  ErrorPage: require('./ErrorPage.js').default
+  Main: require('./Main/index.js').default,
+  Login: require('./Login/index.js').default,
+  Explore: require('./Explore/index.js').default,
+  SinglePlace: require('./SinglePlace/index.js').default,
+  ErrorPage: require('./ErrorPage/index.js').default
 }
 
 export default
