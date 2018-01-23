bda2692385bf71e369e86c9f91750e3919a23745
Tom Schuster <tomjschuster@gmail.com>
2018-01-22 23:34:15 -0500

    cleanup create reducers

--- client/redux/index.js
+++ client/redux/index.js
@@ -16,31 +16,20 @@ export const branches = {
 }
 
 
-export const actions =
-  Object
-    .values(branches)
-    .reduce((actions, branch) => ({
-      ...actions, ...branch.actions
-    }), {})
+export const actions = Object.values(branches).reduce((actions, branch) => (
+  { ...actions, ...branch.actions }
+), {})
 
-const createReducer = ({ initialState, actionHandler }) => {
-  return (state = initialState, action) => {
-    const reduceFn = actionHandler[action.type]
-    if (!reduceFn) { return state }
-    return reduceFn(state, action)
-  }
-}
+const createReducer = ({ initialState, actionHandler : handler }) =>
+  (state = initialState, action) =>
+    handler[action.type] ? handler[action.type](state, action) : state
 
-export const reducers =
-  Object
-    .keys(branches)
-    .reduce((reducers, branch) => ({
-      ...reducers, [branch]: createReducer(branches[branch])
-    }), {})
-
+export const reducers = Object.keys(branches).reduce((reducers, branch) => (
+  { ...reducers, [branch]: createReducer(branches[branch]) }
+), {})
 
 export default createStore(
-    combineReducers(reducers),
-    applyMiddleware(createLogger(), thunk)
-    // applyMiddleware(thunk)
-  )
+  combineReducers(reducers),
+  applyMiddleware(createLogger(), thunk)
+  // applyMiddleware(thunk)
+)
