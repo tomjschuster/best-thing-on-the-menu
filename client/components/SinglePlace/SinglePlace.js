06333d8e7ab858f3ab84c51e9d2733c06e19fbdd
Tom Schuster <tomjschuster@gmail.com>
2018-01-24 19:08:12 -0500

    break SinglePlace into components

--- client/components/SinglePlace/SinglePlace.js
+++ client/components/SinglePlace/SinglePlace.js
@@ -4,6 +4,57 @@ import Item from './Item'
 import AddReview from './AddReview'
 import style from './style.css'
 
+const Header = ({ name, isShowAddReview, showAddReview }) => (
+  <div className={style.nameBox}>
+    <h2>{name}</h2>
+    {isShowAddReview ? null : (
+      <div className={style.addReviewButton}>
+        <Button label="Add a Review" onClick={showAddReview} raised primary />
+      </div>
+    )}
+  </div>
+)
+
+const Address = ({ address }) => (
+  <div className={style.address}>
+    <p>{address}</p>
+  </div>
+)
+const AddReviewBox = ({
+  isShowAddReview,
+  items,
+  addReviewForm,
+  closeAndClearAddReview,
+  updateItemName,
+  updateStars,
+  updateComment,
+  onReviewSubmit
+}) =>
+  isShowAddReview ? (
+    <div className={style.addReview}>
+      <AddReview
+        itemsSource={items ? items.map(({ name }) => name) : []}
+        addReviewForm={addReviewForm}
+        closeAndClearAddReview={closeAndClearAddReview}
+        updateItemName={updateItemName}
+        updateStars={updateStars}
+        updateComment={updateComment}
+        onReviewSubmit={onReviewSubmit}
+      />
+    </div>
+  ) : null
+
+const CurrentPlaceItems = ({ items, toggleItemExpanded }) =>
+  items ? (
+    <div>
+      {items.map(item => (
+        <div key={item.id}>
+          <Item item={item} toggleItemExpanded={toggleItemExpanded} />
+        </div>
+      ))}
+    </div>
+  ) : null
+
 const SinglePlace = ({
   currentPlace,
   ux,
@@ -17,41 +68,26 @@ const SinglePlace = ({
   toggleItemExpanded
 }) => (
   <div className={style.singlePlace}>
-    <div className={style.nameBox}>
-      <h2>{currentPlace.name}</h2>
-      {ux.isShowAddReview ? null : (
-        <div className={style.addReviewButton}>
-          <Button label="Add a Review" onClick={showAddReview} raised primary />
-        </div>
-      )}
-    </div>
-    <div className={style.address}>
-      <p>{currentPlace.address}</p>
-    </div>
-    {ux.isShowAddReview ? (
-      <div className={style.addReview}>
-        <AddReview
-          itemsSource={
-            currentPlace.items ? currentPlace.items.map(({ name }) => name) : []
-          }
-          addReviewForm={forms.addReview}
-          closeAndClearAddReview={closeAndClearAddReview}
-          updateItemName={updateItemName}
-          updateStars={updateStars}
-          updateComment={updateComment}
-          onReviewSubmit={onReviewSubmit}
-        />
-      </div>
-    ) : null}
-    {currentPlace.items ? (
-      <div>
-        {currentPlace.items.map(item => (
-          <div key={item.id}>
-            <Item item={item} toggleItemExpanded={toggleItemExpanded} />
-          </div>
-        ))}
-      </div>
-    ) : null}
+    <Header
+      name={currentPlace.name}
+      isShowAddReview={ux.isShowAddReview}
+      showAddReview={showAddReview}
+    />
+    <Address address={currentPlace.address} />
+    <AddReviewBox
+      isShowAddReview={ux.isShowAddReview}
+      items={currentPlace.items}
+      addReviewForm={forms.addReview}
+      closeAndClearAddReview={closeAndClearAddReview}
+      updateItemName={updateItemName}
+      updateStars={updateStars}
+      updateComment={updateComment}
+      onReviewSubmit={onReviewSubmit}
+    />
+    <CurrentPlaceItems
+      items={currentPlace.items}
+      toggleItemExpanded={toggleItemExpanded}
+    />
   </div>
 )
 
