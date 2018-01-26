0948947d5dc0620727bda595278c5a3a8197f0dd
Tom Schuster <tomjschuster@gmail.com>
2018-01-26 10:56:32 -0500

    fix delete place with reviews

--- db/index.js
+++ db/index.js
@@ -1,8 +1,9 @@
 const mysql = require('promise-mysql')
 
-
-const connectionString = process.env.JAWSDB_MARIA_URL
-  || 'mysql://root@localhost:3306/btotm'
+const connectionString =
+  process.env.NODE_ENV === 'production'
+    ? process.env.JAWSDB_MARIA_URL
+    : process.env.LOCAL_MARIA_URL || 'mysql://root@localhost:3306/btotm'
 
 const paramsString = '?multipleStatements=true&connectionLimit=10&debug=true'
 
@@ -12,4 +13,3 @@ const procs = require('./procs')
 const call = require('./call')(pool, procs)
 
 module.exports = { pool, call }
-
--- db/migrations/sqls/20180123044047-create-proc-delete-place-up.sql
+++ db/migrations/sqls/20180123044047-create-proc-delete-place-up.sql
@@ -1,7 +1,7 @@
 CREATE PROCEDURE deletePlace(place_id INT)
 BEGIN
   DELETE FROM review WHERE item_id IN (
-    SELECT id FROM item i WHERE i.place_id = place_id
+    SELECT id FROM item WHERE place_id = place_id
   );
   DELETE FROM item where place_id = place_id;
   DELETE from place where id = place_id;
